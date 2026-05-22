70b044c156abb27d2b7864a851ed91d2b07a97ce
Vilintuks1 <vilintukas123@gmail.com>
2026-05-22 23:37:42 +0300

    Finished pause menu
    Finished pause menu, might change art later if needed.

---
+++ InputSystem/Assets/Scripts/MainMenu/PauseMenu.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject pauseMenuPanel;      
+    public GameObject optionsMenuPanel;     
+    public GameObject areYouSurePanel;      
+
+    [Header("Buttons")]
+    public GameObject returnBtn;
+    public GameObject settingsBtn;
+    public GameObject exitBtn;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                if (optionsMenuPanel.activeSelf || areYouSurePanel.activeSelf)
+                {
+                    BackToPauseMenu();
+                }
+                else
+                {
+                    ResumeGame();
+                }
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuPanel.SetActive(false);
+        optionsMenuPanel.SetActive(false);
+        areYouSurePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenuPanel.SetActive(true);
+        optionsMenuPanel.SetActive(false);
+        areYouSurePanel.SetActive(false);
+
+        SetMainButtonsActive(true);
+
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void OpenSettings()
+    {
+        SetMainButtonsActive(false);
+        optionsMenuPanel.SetActive(true); 
+    }
+
+    public void OpenAreYouSure()
+    {
+        SetMainButtonsActive(false); 
+        areYouSurePanel.SetActive(true); 
+    }
+
+    public void BackToPauseMenu()
+    {
+        optionsMenuPanel.SetActive(false);
+        areYouSurePanel.SetActive(false);
+        SetMainButtonsActive(true);
+    }
+
+    public void ConfirmExit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    private void SetMainButtonsActive(bool state)
+    {
+        if (returnBtn) returnBtn.SetActive(state);
+        if (settingsBtn) settingsBtn.SetActive(state);
+        if (exitBtn) exitBtn.SetActive(state);
+    }
+}
